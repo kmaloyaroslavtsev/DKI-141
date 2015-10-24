a9a5f4faca172aa41d600143f7531c318b012292
Kirill Maloyaroslavtsev <k.maloyaroslavtsev@pflb.ru>
2015-10-24 14:40:06 +0300

    Начали делать какие-то типы

---
+++ GameProject/GameLogic/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLogic
+{
+    /// <summary>
+    ///     Базовый класс всех карт
+    /// </summary>
+    public abstract  class Card
+    {
+        /// <summary>
+        ///      Название карты
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     Стоимость карты
+        /// </summary>
+        public int Cost { get; set; }
+
+    }
+}
---
+++ GameProject/GameLogic/CreatureCard.cs
@@ -0,0 +1,13 @@
+﻿namespace GameLogic
+{
+    /// <summary>
+    ///     Карты - существа
+    /// </summary>
+    public abstract class CreatureCard : Card
+    {
+        /// <summary>
+        ///     Здоровье карты
+        /// </summary>
+        public int Health { get; set; }
+    }
+}
